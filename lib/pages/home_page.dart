6aebb07d8d9793498846deb94e5e730ed96e9418
RISHITH125 <rishithp125@gmail.com>
2024-04-05 19:59:22 +0530

    debug

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sample/appbars/search_appbar.dart';
+import 'package:sample/components/search_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
